81af68fc7c040cfdf2927ee948a45ee05b5b5537
Guillaume GRONNIER <gglyon769@gmail.com>
2019-07-24 18:54:22 +0200

    add Tor v3 support

--- utils/validation.go
+++ utils/validation.go
@@ -6,6 +6,11 @@ import (
 )
 
 func IsOnion(identifier string) bool {
+	// Add support Tor v3
+	if len(identifier) >= 62 && strings.HasSuffix(identifier, ".onion") {
+		matched, _ := regexp.MatchString(`(^|\.)[a-z2-7]{56}\.onion$`, identifier)
+		return matched
+	}
 	// TODO: At some point we will want to support i2p
 	if len(identifier) >= 22 && strings.HasSuffix(identifier, ".onion") {
 		matched, _ := regexp.MatchString(`(^|\.)[a-z2-7]{16}\.onion$`, identifier)
