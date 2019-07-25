eb0106ffe437615ac12d86dabc085575e9f4b63d
Guillaume GRONNIER <gglyon769@gmail.com>
2019-07-25 10:14:38 +0200

    Update pageparser.go

--- spider/pageparser.go
+++ spider/pageparser.go
@@ -2,8 +2,8 @@ package spider
 
 import (
 	"bytes"
-	"github.com/s-rah/onionscan/model"
-	"github.com/s-rah/onionscan/utils"
+	"github.com/gugronnier/onionscan/model"
+	"github.com/gugronnier/onionscan/utils"
 	"golang.org/x/net/html"
 	"io"
 	"net/url"
