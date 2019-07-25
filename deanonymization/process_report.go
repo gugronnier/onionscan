d7e2039f5a4438e74b1c66d297d49659abdaa00d
Guillaume GRONNIER <gglyon769@gmail.com>
2019-07-25 10:02:29 +0200

    Update process_report.go

--- deanonymization/process_report.go
+++ deanonymization/process_report.go
@@ -1,9 +1,9 @@
 package deanonymization
 
 import (
-	"github.com/s-rah/onionscan/config"
-	"github.com/s-rah/onionscan/report"
-	"github.com/s-rah/onionscan/utils"
+	"github.com/gugronnier/onionscan/config"
+	"github.com/gugronnier/onionscan/report"
+	"github.com/gugronnier/onionscan/utils"
 )
 
 // ExtractIdentifierStep is a pipeline step which extracts as many relationships
