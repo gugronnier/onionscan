7c858ffc4dd25cab6633c0303b43b7c724847efe
Guillaume GRONNIER <gglyon769@gmail.com>
2019-07-25 09:59:01 +0200

    Update pipeline.go

--- onionscan/pipeline.go
+++ onionscan/pipeline.go
@@ -3,8 +3,8 @@ package onionscan
 import (
 	"errors"
 	"fmt"
-	"github.com/s-rah/onionscan/report"
-	"github.com/s-rah/onionscan/utils"
+	"github.com/gugronnier/onionscan/report"
+	"github.com/gugronnier/onionscan/utils"
 	"strings"
 )
 
